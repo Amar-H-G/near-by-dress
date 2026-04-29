8ac861ac5c29a6a854155c270d2ee1b8a92259ab
Amar Patra <amarpatra932@gmail.com>
2026-04-29 10:22:54 +0530

    Product part done

--- backend/src/modules/seller/controllers/seller.controller.js
+++ backend/src/modules/seller/controllers/seller.controller.js
@@ -1,37 +1,37 @@
 const sellerService = require('../services/seller.service');
-const { sendResponse } = require('../../../utils/response');
+const { sendSuccess } = require('../../../utils/response');
 
 exports.getDashboard = async (req, res) => {
   const stats = await sellerService.getDashboardStats(req.user._id);
-  sendResponse(res, 200, stats);
+  sendSuccess(res, { data: stats });
 };
 
 exports.getProfile = async (req, res) => {
   const profile = await sellerService.getSellerProfile(req.user._id);
-  sendResponse(res, 200, { profile });
+  sendSuccess(res, { data: { profile } });
 };
 
 exports.updateProfile = async (req, res) => {
   const profile = await sellerService.upsertSellerProfile(req.user._id, req.body, req.files);
-  sendResponse(res, 200, { profile, message: 'Profile updated successfully' });
+  sendSuccess(res, { data: { profile } }, 'Profile updated successfully');
 };
 
 exports.getProducts = async (req, res) => {
   const result = await sellerService.getSellerProducts(req.user._id, req.query);
-  sendResponse(res, 200, result);
+  sendSuccess(res, { data: result });
 };
 
 exports.addProduct = async (req, res) => {
   const product = await sellerService.addSellerProduct(req.user._id, req.body, req.files);
-  sendResponse(res, 201, { product, message: 'Product added successfully' });
+  sendSuccess(res, { data: { product } }, 'Product added successfully', 201);
 };
 
 exports.updateProduct = async (req, res) => {
   const product = await sellerService.updateSellerProduct(req.user._id, req.params.id, req.body, req.files);
-  sendResponse(res, 200, { product, message: 'Product updated successfully' });
+  sendSuccess(res, { data: { product } }, 'Product updated successfully');
 };
 
 exports.deleteProduct = async (req, res) => {
   await sellerService.deleteSellerProduct(req.user._id, req.params.id);
-  sendResponse(res, 200, null, 'Product deleted successfully');
+  sendSuccess(res, null, 'Product deleted successfully');
 };
